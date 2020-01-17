095c95d44fc3214a87bdbe6f2dc56be8d473e728
sitzmbry000 <sitzmbry000@warren.k12.in.us>
2020-01-17 10:58:53 -0500

    update v1

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-let changeBox = function(){
+let changeBackground = function(){
 
     let box1 = document.getElementById("block1");
+    let box2 = document.getElementById("block2");
+    let box3 = document.getElementById("block3");
+    let box4 = document.getElementById("block4");
+    let box5 = document.getElementById("block5");
+    let box6 = document.getElementById("block6");
+    let box7 = document.getElementById("block7");
+    let box8 = document.getElementById("block8");
+    let box9 = document.getElementById("block9");
+
+    box1.style.backgroundColor = "blue";
+    box2.style.backgroundColor = "blue";
+    box3.style.backgroundColor = "blue";
+    box4.style.backgroundColor = "blue";
+    box5.style.backgroundColor = "blue";
+    box6.style.backgroundColor = "blue";
+    box7.style.backgroundColor = "blue";
+    box8.style.backgroundColor = "blue";
+    box9.style.backgroundColor = "blue";
+
+
 };
